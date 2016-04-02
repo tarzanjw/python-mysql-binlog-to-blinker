a27299152d4e25f734be99238236c0f89bac86c6
Tarzan <hoc3010@gmail.com>
2016-04-02 11:25:30 +0700

    edit example

--- examples/simple.py
+++ examples/simple.py
@@ -25,48 +25,21 @@ __author__ = 'tarzan'
 _logger = logging.getLogger(__name__)
 
 
-n_event = 0
-
-
-def on_binlog_event(e, stream):
-    global n_event
-    n_event += 1
-    if n_event >= 20:
-        raise SystemExit()
-
-
 @signals.on_rows_updated
 def on_rows_updated(table, rows, meta):
-    from datetime import datetime
-    for row in rows:
-        print(datetime.fromtimestamp(meta['time']),
-              row['values']['use_id'],
-              row['values']['use_loginname'])
-        if 'use_loginname' not in row['updated_values']:
-            continue
-        before, after = row['updated_values']['use_loginname']
-        if not ('inoxtuananh' in before or 'inoxtuananh' in after):
-            continue
-        print('\n', '*' * 32, '\n')
-        print(row['values']['use_id'], row['values']['use_loginname'])
-        print(row['updated_values'])
-        print('SOS ' * 64)
+    print('ROWS UPDATED')
+    print(table)
+    print(meta)
+    print(rows)
 
 
 def main():
-    only_tables = ['orders_new', ]
-    only_tables = ['users', ]
-    # for i in range(20):
-    #     only_tables.append('orders_product_%d' % i)
     mysqlbinlog2blinker.start_replication(
         {
             'host': 'localhost',
-            'user': 'binlog_publisher',
-            'passwd': 'EWwjGWf9U346',
-            'port': 33060,
+            'user': 'root',
+            'port': 3306,
         },
-        only_tables=only_tables,
-        blocking=False,
     )
 
 
